c8177d7f82d4549dd0cc10ea9172ddd93a799357
Justinel2 <justine.lardeux@hotmail.com>
2019-02-05 11:42:01 -0500

    P1: Restart project 1

---
+++ projects/project1/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*****************
+
+******************/
+
+
+// When the document is loaded we call the setup function
+$(document).ready(setup);
+
+// setup()
+//
+// Sets the click handler and starts the time loop
+function setup() {
+
+};
