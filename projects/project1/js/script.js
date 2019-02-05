e54915a7308674298952a329a9343a2ddb8d4116
Justinel2 <justine.lardeux@hotmail.com>
2019-02-05 16:54:32 -0500

    P1: JS - Declare and define variables for the divs on page
    - table
    - sink
    - recycling
    - trash

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -4,31 +4,35 @@
 
 ******************/
 
-var objects = [];
+let $objects = [];
+let $table;
+let $sink;
+let $recycling;
+let $trash;
 
-objects[1] = "milk carton";
-objects[2] = "soda can";
-objects[3] = "egg carton";
-objects[4] = "tea bag";
-objects[5] = "banana peel";
-objects[6] = "apple core";
-objects[7] = "receipt";
-objects[8] = "dirty diaper";
-objects[9] = "cat liter";
-objects[10] = "paper";
-objects[11] = "beer bottle";
-objects[12] = "cereal box";
-objects[13] = "newspaper";
-objects[14] = "soup can";
-objects[15] = "old fish";
-objects[16] = "used tissue";
-objects[17] = "tattered sock";
-objects[18] = "gift wrapping";
-objects[19] = "sticky note";
-objects[20] = "lasagna";
-objects[21] = "water bottle";
-objects[22] = "bubble wrap";
-objects[23] = "big box";
+$objects[1] = "milk carton";
+$objects[2] = "soda can";
+$objects[3] = "egg carton";
+$objects[4] = "tea bag";
+$objects[5] = "banana peel";
+$objects[6] = "apple core";
+$objects[7] = "receipt";
+$objects[8] = "dirty diaper";
+$objects[9] = "cat liter";
+$objects[10] = "paper";
+$objects[11] = "beer bottle";
+$objects[12] = "cereal box";
+$objects[13] = "newspaper";
+$objects[14] = "soup can";
+$objects[15] = "old fish";
+$objects[16] = "used tissue";
+$objects[17] = "tattered sock";
+$objects[18] = "gift wrapping";
+$objects[19] = "sticky note";
+$objects[20] = "lasagna";
+$objects[21] = "water bottle";
+$objects[22] = "bubble wrap";
+$objects[23] = "big box";
 
 // When the document is loaded we call the setup function
 $(document).ready(setup);
@@ -38,5 +42,13 @@ $(document).ready(setup);
 // Sets the click handler and starts the time loop
 function setup() {
 
-  
+  $table = $("div#table");
+  $sink = $("div#sink");
+  $recycling = $("div#recycling");
+  $trash = $("div#trash");
+
+  for (var i = 0; i < $objects.length; i++) {
+    $("#table").append('<div id="newDiv"></div>');
+  }
+
 };
