d61d75421ff00b290608a0391006345d6d9d7942
Justinel2 <justine.lardeux@hotmail.com>
2019-02-28 14:30:55 -0500

    P2: Starting project 2

---
+++ projects/project2/js/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/*****************
+
+I AM NOT AGAINST RECYCLING
+By Justine Lardeux (40030920)
+
+This program differenciates the different items to discard. The user can place those items in the
+sink (to clean), in the recycling and in the trash. When all the items are sorted, the trashes
+are sent to be processed...
+
+******************/
+
+// Variables
+
+
+
+// When the document is loaded we call the setup function
+$(document).ready(setup);
+
+
+// setup()
+//
+// Function initiating the program
+function setup() {
+
+  };
+
+// startGame()
+//
+// A function that hides the introduction page when the start button is clicked.
+// By that, the gameplay is visible (z-index)
+// function startGame(){
+//   $('.intro').css('display', 'none');
+// }
