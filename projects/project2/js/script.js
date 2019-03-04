af2e7f2bf0e9a0aeaa40c28af0fff41e69bd63e3
Justinel2 <justine.lardeux@hotmail.com>
2019-03-04 12:19:34 -0500

    P2: script -> Establish basis of generateWebPage()

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -8,13 +8,14 @@
 
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 var content;
-var url;
+var url = "http://boards.4channel.org/";
 var links = ["a", "c", "g", "k", "m", "o", "p", "v", "vg", "vr",
-             "w", "vip", "qa", "cm", "lgbt", "3", "adv", "an"
-             "asp", "biz", "cgl", "ck", "co", "diy", "fa", "fit"
-             "gd", "his", "int", "jp", "lit", "mlp", "mu", "n"
-             "news", "out", "po", "qst", "sci", "sp", "tg", "toy"
+             "w", "vip", "qa", "cm", "lgbt", "3", "adv", "an",
+             "asp", "biz", "cgl", "ck", "co", "diy", "fa", "fit",
+             "gd", "his", "int", "jp", "lit", "mlp", "mu", "n",
+             "news", "out", "po", "qst", "sci", "sp", "tg", "toy",
              "trv", "tv", "vp", "wsg", "wsr", "x"];
+var browserOpen = false;
 
 let $signInPage;
 let $homePage;
@@ -74,16 +75,40 @@ function signIn() {
 }
 
 function openInternet() {
-
   $webPage.css('display', 'block');
+
+  if (browserOpen === false) {
+    generateWebPage();
+    browserOpen = true;
+  }
 }
 
 function openJournal() {
   $journalPage.css('display', 'block');
+  // NOT IN OPEN JOURNAL if (browserOpen >= 1)... do it + end the day + put all to 0
 }
 
 function closePage() {
   $(this).parent().parent().css('display', 'none');
+}
+
+function generateWebPage() {
+  console.log("generate");
+  fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+  .then((res) => {
+      return res.text();
+  })
+  .then((data) => {
+    content = String(data);
+    console.log(content);
+    $('#para').html(content);
+    $(function(){
+      $('a').each(function() {
+        $(this).attr('href', ' ');
+      });
+    });
+   })
+  .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
 }
 
 // startGame()
